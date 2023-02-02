98cb8323f634b947f4910229b211456293c346d9
Alberto Elorza <albertoelorza2002@gmail.com>
2023-02-02 14:03:23 +0100

    include else keyword

--- compiler/parser/tokenizer.ts
+++ compiler/parser/tokenizer.ts
@@ -8,7 +8,7 @@ export type Token = {
 
 export const Keywords = [
   'def', 'except', 'extend', 'open', 'use', 'it',
-  'if','then', 'end', 'struct', 'priv', 'as',
+  'if','then', 'else', 'end', 'struct', 'priv', 'as',
   'into', 'this', 'ensured', 'implicit', 'true', 'false',
   '(', ')', '[', ']', '{', '}', ';', ',', '.', ':', '_'
 ] as const
