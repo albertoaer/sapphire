181ab5a863f71633225a6ede2e4678cee1ed33a5
Alberto Elorza <albertoelorza2002@gmail.com>
2023-02-08 21:32:38 +0100

    included push if method

--- compiler/wasm/expressions.ts
+++ compiler/wasm/expressions.ts
@@ -1,11 +1,8 @@
 import { CompilerError } from './common.ts';
+import { WasmType } from './module.ts';
 
 export class WasmExpression {
-  readonly arr: number[] = [];
-
-  get code() {
-    return new Uint8Array(this.arr);
-  }
+  private readonly data: number[]
 
   private ensureBytes(bytes: number[]): number[] {
     if (!bytes.every(x => Number.isInteger(x) && x >= 0 && x <= 255))
@@ -13,7 +10,22 @@ export class WasmExpression {
     return bytes;
   }
 
-  rawPush(...bytes: number[]) {
-    this.arr.push(...this.ensureBytes(bytes));
+  constructor(...data: number[]) {
+    this.data = this.ensureBytes(data);
+  }
+
+  get code(): Uint8Array {
+    return new Uint8Array(this.data);
+  }
+
+  pushRaw(...bytes: number[]) {
+    this.data.push(...this.ensureBytes(bytes));
+  }
+
+  pushIf(cond: WasmExpression, type: WasmType | null, branch: WasmExpression, elseBranch?: WasmExpression) {
+    if (elseBranch !== undefined)
+      this.data.push(...cond.code, 0x04, type ?? 0x40, ...branch.code, 0x05, ...elseBranch.code, 0x0b);
+    else
+      this.data.push(...cond.code, 0x04, type ?? 0x40, ...branch.code, 0x0b);
   }
 }
