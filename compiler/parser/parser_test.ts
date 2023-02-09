32ba7e61626f630e050ed19c4c09e553edf988ed
Alberto Elorza <albertoelorza2002@gmail.com>
2023-02-09 22:44:44 +0100

    update module parser

--- compiler/parser/parser_test.ts
+++ compiler/parser/parser_test.ts
@@ -1,76 +1,79 @@
 import { assertThrows, assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
-import { createParserFor, modules } from "./common_test.ts";
+import { createParserFor } from "./common_test.ts";
 
 Deno.test('must parse', () => {
   assertEquals(createParserFor('int{4}').parseType(),
-    { base: ['int'], line: 1, array: { size: 4 } }
+    { base: ['int'], meta: { line: 1 }, array: { size: 4 } }
   ); 
   assertEquals(createParserFor('[int{}, float]').parseType(),
     { base: [
-      { base: ['int'], line: 1, array: { size: undefined} },{ base: ['float'], line: 1, array: undefined }
-      ], line: 1, array: undefined
+      { base: ['int'], meta: { line: 1 }, array: { size: undefined} },
+      { base: ['float'], meta: { line: 1 }, array: undefined }
+      ], meta: { line: 1 }, array: undefined
     }
   ); 
   assertEquals(createParserFor('.b.c').parseName('a'), ['a', 'b', 'c']); 
   assertEquals(createParserFor('int a, float{}, _ var)').parseHeuristicList({ value: ')' }), [
-    { name: 'a', type: { base: ['int'], line: 1, array: undefined } },
-    { name: null, type: { base: ['float'], line: 1, array: { size: undefined } }},
-    { name: 'var', type: null },
+    { name: 'a', meta: { line: 1 }, type: { base: ['int'],  meta: { line: 1 }, array: undefined } },
+    { name: null, meta: { line: 1 }, type: { base: ['float'],  meta: { line: 1 }, array: { size: undefined } }},
+    { name: 'var', meta: { line: 1 }, type: null },
   ]);
   {
     const expr = createParserFor('int x, string y) () . end');
     assertEquals(expr.parseArgList({ value: ')' }), [
-      { name: 'x', type: { base: ['int'], line: 1, array: undefined } },
-      { name: 'y', type: { base: ['string'], line: 1, array: undefined } },
+      { name: 'x', meta: { line: 1 }, type: { base: ['int'],  meta: { line: 1 }, array: undefined } },
+      { name: 'y', meta: { line: 1 }, type: { base: ['string'], meta: { line: 1 }, array: undefined } },
     ]);
     assertEquals(expr.remain(), ['(', ')', '.', 'end'].map(x => { return { type: 'keyword', value: x, line: 1 } }));
   }
-  assertEquals(createParserFor('true').tryParseLiteral(), { type: modules['kernel']['defs']['bool'], value: 'true' });
+  assertEquals(createParserFor('true').tryParseLiteral(), { type: 'bool', meta: { line: 1 }, value: 'true' });
   assertEquals(createParserFor('! . + .').parseExpression(), {
-    id: 'call', func: ['+'], line: 1,
-    args: [{ id: 'call', func: ['!'], args: [{ id: 'none', line: 1 }], line: 1 }, { id: 'none', line: 1 }]
+    id: 'call', func: ['+'], meta: { line: 1 },
+    args: [{ id: 'call', func: ['!'], args: [{ id: 'none', meta: { line: 1 } }], meta: { line: 1 } }, { id: 'none', meta: { line: 1 } }]
   });
   assertEquals(createParserFor('a.b(f.g, 3) * .').parseExpression(), { id: 'call', func: ['*'], args: [
     {
-      id: 'call', func: ['a', 'b'], line: 1,
+      id: 'call', func: ['a', 'b'], meta: { line: 1 },
       args: [
-        { id: 'value', of: ['f', 'g'], line: 1 },
-        { id: 'literal', value: { type: modules['kernel']['defs']['int'], value: '3' }, line: 1 }
+        { id: 'value', of: ['f', 'g'], meta: { line: 1 }},
+        { id: 'literal', meta: { line: 1 }, value: { type: 'int', value: '3', meta: { line: 1 } } }
       ]
     },
-    { id: 'none', line: 1 }
-  ], line: 1 });
+    { id: 'none', meta: { line: 1 } }
+  ], meta: { line: 1 } });
   assertEquals(createParserFor('++a.b.c(false) / .').parseExpression(), { id: 'call', func: ['/'], args: [
     { id: 'call', func: ['++'], args: [
       { id: 'call', func: ['a', 'b', 'c'], args: [
-        { id: 'literal', value: { type: modules['kernel']['defs']['bool'], value: 'false' }, line: 1 }
-      ], line: 1 }
-    ], line: 1 },
-    { id: 'none', line: 1 }
-  ], line: 1 });
+        { id: 'literal', value: { type: 'bool', value: 'false', meta: { line: 1 } }, meta: { line: 1 } }
+      ], meta: { line: 1 } }
+    ], meta: { line: 1 } },
+    { id: 'none', meta: { line: 1 } }
+  ], meta: { line: 1 } });
   assertEquals(createParserFor('2 + if smt.fn() then 1 else 0 end').parseExpression(), { id: 'call', func: ['+'], args: [
-    { id: 'literal', value: { type: modules['kernel']['defs']['int'], value: '2' }, line: 1 },
-    { id: 'if', line: 1,
-      cond: { id: 'call', func: ['smt', 'fn'], args: [], line: 1 },
-      then: { id: 'literal', line: 1, value: { type: modules['kernel']['defs']['int'], value: '1' } },
-      else: { id: 'literal', line: 1, value: { type: modules['kernel']['defs']['int'], value: '0' } } }
-  ], line: 1 });
+    { id: 'literal', value: { type: 'int', value: '2', meta: { line: 1 } }, meta: { line: 1 } },
+    { id: 'if', meta: { line: 1 },
+      cond: { id: 'call', func: ['smt', 'fn'], args: [], meta: { line: 1 } },
+      then: { id: 'literal', meta: { line: 1 }, value: { type: 'int', value: '1', meta: { line: 1 } } },
+      else: { id: 'literal', meta: { line: 1 }, value: { type: 'int', value: '0', meta: { line: 1 } } }
+    }
+  ], meta: { line: 1 } });
   assertEquals(createParserFor('if a then b else if c then d else e end').parseExpression(), {
-    id: 'if', line: 1,
-    cond: { id: 'value', of: ['a'], line: 1 },
-    then: { id: 'value', of: ['b'], line: 1 },
+    id: 'if', meta: { line: 1 },
+    cond: { id: 'value', of: ['a'], meta: { line: 1 } },
+    then: { id: 'value', of: ['b'], meta: { line: 1 } },
     else: {
-      id: 'if', line: 1,
-      cond: { id: 'value', of: ['c'], line: 1 },
-      then: { id: 'value', of: ['d'], line: 1 },
-      else: { id: 'value', of: ['e'], line: 1 }
+      id: 'if', meta: { line: 1 },
+      cond: { id: 'value', of: ['c'], meta: { line: 1 } },
+      then: { id: 'value', of: ['d'], meta: { line: 1 } },
+      else: { id: 'value', of: ['e'], meta: { line: 1 } }
     }
   });
-  assertEquals(createParserFor('a.c[b.c(),2].x.y').parseExpression(), {
-    id: 'get', name: ['x', 'y'], line: 1, origin: {
-      id: 'index', line: 1, origin: { id: 'value', of: ['a', 'c'], line: 1 }, args: [
-        { id: 'call', func: ['b', 'c'], args: [], line: 1 },
-        { id: 'literal', value: { value: '2', type: modules['kernel']['defs']['int'] }, line: 1 }
+  assertEquals(createParserFor('a.c[b.c(),2,"hello"].x.y').parseExpression(), {
+    id: 'get', name: ['x', 'y'], meta: { line: 1 }, origin: {
+      id: 'index', meta: { line: 1 }, origin: { id: 'value', of: ['a', 'c'], meta: { line: 1 } }, args: [
+        { id: 'call', func: ['b', 'c'], args: [], meta: { line: 1 } },
+        { id: 'literal', value: { value: '2', type: 'int', meta: { line: 1 } }, meta: { line: 1 } },
+        { id: 'literal', value: { value: 'hello', type: 'string', meta: { line: 1 } }, meta: { line: 1 } }
       ]
     }
   });
