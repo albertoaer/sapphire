d1798c7fa2408afd67a78a7b3c9e7574d46bf005
Alberto Elorza <albertoelorza2002@gmail.com>
2023-02-20 18:40:57 +0100

    better errors

--- compiler/errors.ts
+++ compiler/errors.ts
@@ -1,41 +1,47 @@
 import { Type } from "./sapp.ts";
 
-export class ParserError extends Error {
+abstract class GenericError extends Error {
+  constructor(head: string, line: number | null, msg: string) {
+    super(head + (line !== null ? ` at line ${line}: ` : ': ') + msg);
+  }
+}
+
+export class ParserError extends GenericError {
   constructor(line: number, msg: string) {
-    super(`Parser error, Line ${line}: ${msg}`);
+    super('Parser Error', line, msg);
   }
 }
 
 export type RegisteredUnsupportedFeature =
   'Attribute Access' | 'Struct Building' | 'Indexation' | 'Call Returned Function' | 'Function Attributes' |
-  'Function Tables' | 'Native Functions'
+  'Function Tables' | 'Native Functions' | 'Strings'
 
-export class FeatureError extends Error {
-  constructor(line: number, feature: RegisteredUnsupportedFeature) {
-    super(`Error at line ${line}, feature "${feature}" is not supported yet`);
+export class FeatureError extends GenericError {
+  constructor(line: number | null, feature: RegisteredUnsupportedFeature) {
+    super('Feature Error', line, `"${feature}" is not supported yet`);
   }
 }
 
-export class MatchTypeError extends Error {
+export class MatchTypeError extends GenericError {
   constructor(line: number, a: Type, b: Type) {
-    super(`TypeError at line ${line}, ${a.toString()} expected to be ${b.toString()}`);
+    super('Type Error', line, `${a.toString()} expected to be ${b.toString()}`);
   }
 }
 
-export class DependencyError extends Error {
+export class DependencyError extends GenericError {
   constructor(msg: string) {
-    super(msg);
+    super('Dependency Error', null, msg);
   }
 }
 
-export class CompilerError extends Error {
+export class CompilerError extends GenericError {
   constructor(compiler: 'Wasm', msg: string) {
-    super(`${compiler} compiler error: ${msg}`);
+    super(`${compiler} Compiler Error`, null, msg);
   }
 }
 
-export class IOError extends Error {
+export class IOError extends GenericError {
   constructor(msg: string) {
-    super(`IO error: ${msg}`);
+    super('IO Error', null, msg);
   }
 }
