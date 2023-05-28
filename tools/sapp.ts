4d843587b4c12bce913efad75425463103eb5265
Alberto Elorza <albertoelorza2002@gmail.com>
2023-05-28 17:57:35 +0200

    better info

--- tools/sapp.ts
+++ tools/sapp.ts
@@ -1,9 +1,15 @@
-/**
- * sapp is prepared for running any Sapphire compatible file
- * That includes:
- *  - source .sa files
- *  - wasm compiled
- */
+const INFO = `
+  Sapp is prepared for running any Sapphire compatible file
+    That includes:
+      - source .sa files
+      - wasm compiled [not yet!]
+    
+    How to use it:
+      $ sapp <SOURCE> [...args]
+
+    Location:
+      ${Deno.execPath()}
+`
 import type { Compiler } from '../compiler/compiler.ts';
 import { WasmCompiler } from '../compiler/sapp_wasm/mod.ts';
 import { WasmVM } from '../compiler/wasm_vm/mod.ts';
@@ -11,19 +17,23 @@ import { FileSystemModuleProvider } from '../compiler/deps.ts';
 
 const [file, ...args] = Deno.args;
 
-const fsp = new FileSystemModuleProvider();
-const compiler: Compiler = new WasmCompiler(fsp);
-const entry_point = "main0";
+if (!file) {
+  console.log(INFO);
+} else {
+  const fsp = new FileSystemModuleProvider();
+  const compiler: Compiler = new WasmCompiler(fsp);
+  const entry_point = "main0";
 
-try {
-  
-  const code = compiler.compile(file);
-  const vm = await WasmVM.create(code);
-  const fn = vm.exports[entry_point] as CallableFunction | undefined;
+  try {
+    
+    const code = compiler.compile(file);
+    const vm = await WasmVM.create(code);
+    const fn = vm.exports[entry_point] as CallableFunction | undefined;
 
-  if (!fn) throw new Error(`Expecting entry point ${entry_point}`);
-  else fn(args);
+    if (!fn) throw new Error(`Expecting entry point ${entry_point}`);
+    else fn(args);
 
-} catch (err) {
-  console.error(err);
+  } catch (err) {
+    console.error(err);
+  }
 }
